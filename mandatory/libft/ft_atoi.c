c32a86818b86a8674adc390c27dfe0d18d4b7bf5
mossab errahal <merrahal@student.1337.ma>
2024-09-28 18:08:26 +0100

    some changes that fits my project

--- mandatory/libft/ft_atoi.c
+++ mandatory/libft/ft_atoi.c
@@ -6,17 +6,25 @@
 /*   By: merrahal <merrahal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 02:57:49 by merrahal          #+#    #+#             */
-/*   Updated: 2024/03/02 16:00:37 by merrahal         ###   ########.fr       */
+/*   Updated: 2024/09/28 12:32:02 by merrahal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h" 
 
-int	ft_atoi(const char *str)
+void check_result(long result, int sign)
 {
-	int	i;
+	if(sign == -1)
+		return (ft_putstr_fd(RED"Error : negativ nbr\n"ENDC, 2), _free(), exit(1));
+	if(result > 255)
+		return (ft_putstr_fd(RED"Error : more then 255\n"ENDC, 2), _free(), exit(1));
+}
+
+int	ft_atoi(char *str)
+{
+	unsigned long	i;
 	int	sign;
-	int	result;
+	unsigned long	result;
 
 	i = 0;
 	sign = 1;
@@ -29,9 +37,12 @@ int	ft_atoi(const char *str)
 			sign *= -1;
 		i++;
 	}
-	while (str[i] <= '9' && str[i] >= '0')
+	printf("str = {%s}\n", str);	
+	while (!ft_isalpha(str, str[i]) && str[i] <= '9' && str[i] >= '0')
 	{
+		printf("c = (%c)\n", str[i]);
 		result = result * 10 + str[i] - '0';
+		check_result(result, sign);
 		i++;
 	}
 	return (result * sign);
--- mandatory/libft/ft_isalpha.c
+++ mandatory/libft/ft_isalpha.c
@@ -6,16 +6,27 @@
 /*   By: merrahal <merrahal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 18:54:06 by merrahal          #+#    #+#             */
-/*   Updated: 2024/03/02 16:00:52 by merrahal         ###   ########.fr       */
+/*   Updated: 2024/09/28 12:33:19 by merrahal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha(int c)
+int	ft_isalpha(char *str, int c)
 {
-	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
+	if((str[0] == '+' || str[0] == '-') && str[1] == '\0')
+	{
+		printf("bad character in c or f\n");
+		_free();
+		exit(1);
+	}
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A') || c == '+' || c == '-')
+	{
+		printf("bad character in c or f\n");
+		_free();
+		exit(1);
 		return (1);
+	}
 	else
 		return (0);
 }
